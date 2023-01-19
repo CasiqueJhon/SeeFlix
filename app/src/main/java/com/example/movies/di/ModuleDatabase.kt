2625190315896464f7f04d9673c9838365845996
jhonisaac.casique@avantgardeit.es <jicasiqueg@eservicios.indra.es>
2023-01-19 19:05:54 +0100

    fix error missingBinding

--- app/src/main/java/com/example/movies/di/ModuleDatabase.kt
+++ app/src/main/java/com/example/movies/di/ModuleDatabase.kt
@@ -9,10 +9,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object ModuleDatabase {
 
     @Provides
